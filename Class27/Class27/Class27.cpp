f5a89d7652ebb652832540fbda38a0644e0b3492
User <kenobi88@gmail.com>
2024-12-05 17:08:28 +0200

    2-commit

--- Class27/Class27/Class27.cpp
+++ Class27/Class27/Class27.cpp
@@ -6,222 +6,124 @@ class MyInt
 private:
 	int value;
 public:
-	MyInt(int value)
-	{
-		this->value = value;
-	}
-	void SetData(int value)
-	{
-		this->value = value;
-	}
-	int GetData() const
-	{
-		return value;
-	}
+	MyInt(int value = 0) : value(value) {}
+
+	void SetData(int value) { this->value = value; }
+	int GetData() const { return value; }
 };
 
+template <typename T>
 class Node
 {
 private:
-	MyInt myInt;
-	Node* next /*= nullptr*/;
+	T data;
+	Node* next;
 public:
-	Node(MyInt myInt) : myInt(myInt), next(nullptr) {}
-	MyInt GetMyInt() const
-	{
-		return myInt;
-	}
-	void SetMyInt(MyInt myInt)
-	{
-		this->myInt = myInt;
-	}
-	Node* GetNext() const
-	{
-		return next;
-	}
-	void SetNext(Node* next)
-	{
-		this->next = next;
-	}
+	Node(T data) : data(data), next(nullptr) {}
+
+	T& GetData() { return data; }
+	void SetData(T data) { this->data = data; }
+
+	Node* GetNext() const { return next; }
+	void SetNext(Node* next) { this->next = next; }
 };
 
-class LinkedList
+template <typename T>
+class Queue
 {
 private:
-	Node* head;
-	size_t size;
+    Node<T>* first;
+    Node<T>* last;
+    size_t size;
 public:
-	LinkedList() : head(nullptr), size(0) {}
-	size_t GetSize() const
-	{
-		return size;
-	}
-	void InsertAtEnd(MyInt value)
-	{
-		if (head == nullptr)
-		{
-			InsertAtBeginning(value);
-			return;
-		}
-		Node* newNode = new Node(value);
+    Queue() : first(nullptr), last(nullptr), size(0) {}
 
-		Node* temp = head;
-		while (temp->GetNext() != nullptr)
-		{
-			temp = temp->GetNext();
-		}
-		temp->SetNext(newNode);
-		size++;
-	}
-	void InsertAtBeginning(MyInt value)
-	{
-		Node* newNode = new Node(value);
-		newNode->SetNext(head);
-		head = newNode;
-		size++;
-	}
+    size_t Size() const { return size; }
 
-	void DeleteFromBeginning()
-	{
-		if (head == nullptr)
-		{
-			cout << "List is empty!" << endl;
-			return;
-		}
-		Node* temp = head;
-		head = head->GetNext();
-		delete temp;
-		size--;
-	}
+    void Enqueue(T data) // додавання елементів у кінець черги
+    {
+        Node<T>* newNode = new Node<T>(data);
+        if (last == nullptr)
+        {
+            first = newNode;
+            last = newNode;
+        }
+        else
+        {
+            last->SetNext(newNode);
+            last = newNode;
+        }
+        size++;
+    }
 
-	void DeleteFromEnd()
-	{
-		if (head == nullptr)
-		{
-			cout << "List is empty!" << endl;
-			return;
-		}
-		if (head->GetNext() == nullptr)
-		{
-			delete head;
-			head = nullptr;
-			size--;
-			return;
-		}
-		Node* temp = head;
-		while (temp->GetNext()->GetNext() != nullptr)
-		{
-			temp = temp->GetNext();
-		}
-		delete temp->GetNext();
-		temp->SetNext(nullptr);
-		size--;
-	}
+    T& Front() // отримання першого елемента
+    {
+        if (first == nullptr)
+        {
+            throw runtime_error("Queue is empty!");
+        }
+        return first->GetData();
+    }
 
-	MyInt GetIndex(size_t index) const
-	{
-		if (index >= size)
-		{
-			throw out_of_range("Wrong index, Carl!");
-		}
-		Node* temp = head;
-		for (size_t i = 0; i < index; i++)
-		{
-			temp = temp->GetNext();
-		}
-		return temp->GetMyInt();
-	}
+    const T Front() const // для константного об'єкта
+    {
+        if (first == nullptr)
+        {
+            throw runtime_error("Queue is empty!");
+        }
+        return first->GetData();
+    }
 
-	void PrintList() const
-	{
-		Node* temp = head;
-		while (temp != nullptr)
-		{
-			cout << temp->GetMyInt().GetData() << " ";
-			temp = temp->GetNext();
-		}
-		cout << endl;
-	}
+    void Dequeue() // видалення першого елемента з черги
+    {
+        if (first == nullptr)
+        {
+            return;
+        }
+        Node<T>* temp = first;
+        first = first->GetNext();
+        if (first == nullptr)
+        {
+            last = nullptr;
+        }
+        delete temp;
+        size--;
+    }
 
-
-	~LinkedList()
-	{
-		while (head != nullptr)
-		{
-			Node* temp = head;
-			head = head->GetNext();
-			delete temp;
-		}
-	}
-
-	class Iterator
-	{
-	private:
-		Node* current;
-	public:
-		Iterator(Node* start) : current(start) {}
-
-		bool HasNext() const
-		{
-			return current != nullptr;
-		}
-
-		MyInt Next()
-		{
-			if (!HasNext())
-			{
-				throw out_of_range("No more elements in the list");
-			}
-			MyInt value = current->GetMyInt();
-			current = current->GetNext();
-			return value;
-		}
-	};
-
-	Iterator GetIterator() const
-	{
-		return Iterator(head);
-	}
+    ~Queue()
+    {
+        while (first != nullptr)
+        {
+            Node<T>* temp = first;
+            first = first->GetNext();
+            delete temp;
+        }
+    }
 };
-
-
 int main()
 {
-	LinkedList* list = new LinkedList();
-	list->InsertAtBeginning(MyInt(5));
-	list->InsertAtEnd(MyInt(7));
-	list->InsertAtEnd(MyInt(9));
+    Queue<MyInt> queue;
+    queue.Enqueue(MyInt(9));
+    queue.Enqueue(MyInt(1));
+    queue.Enqueue(MyInt(5));
+    queue.Enqueue(MyInt(7));
+    queue.Enqueue(MyInt(3));
 
-	cout << "List after adding elements: ";
-	list->PrintList();
+    for (int i = queue.Size(); i > 0; i--)
+    {
+        cout << queue.Front().GetData() << endl;
+        queue.Dequeue();
+    }
 
-	list->DeleteFromBeginning();
-	cout << "List after deleting from beginning: ";
-	list->PrintList();
+    // Черга для int
+    Queue<int> intQueue;
+    intQueue.Enqueue(10);
+    intQueue.Enqueue(20);
+    intQueue.Enqueue(30);
 
-	list->DeleteFromEnd();
-	cout << "List after deleting from end: ";
-	list->PrintList();
-
-	try
-	{
-		cout << "Element at 0: " << list->GetIndex(0).GetData() << endl;
-	}
-	catch (const out_of_range& e)
-	{
-		cout << e.what() << endl;
-	}
-
-	cout << "Iteration go go go: ";
-	LinkedList::Iterator it = list->GetIterator();
-	while (it.HasNext())
-	{
-		cout << it.Next().GetData() << " ";
-	}
-	cout << endl;
-
-	delete list;
-
-	// індексація
-	// вивід елементів через цикл 
+    while (intQueue.Size() > 0)
+    {
+        cout << intQueue.Front() << endl;
+        intQueue.Dequeue();
+    }
 }
