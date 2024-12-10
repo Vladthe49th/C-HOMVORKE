7e17743f71a71a4c75dacf24c1087cec65b7d4cd
User <kenobi88@gmail.com>
2024-12-10 12:27:12 +0200

    4-commit

--- Class27/Class27/Class27.cpp
+++ Class27/Class27/Class27.cpp
@@ -1,31 +1,58 @@
 #include <iostream>
 using namespace std;
 
+
 class MyInt
 {
 private:
-	int value;
+    int value;
 public:
-	MyInt(int value = 0) : value(value) {}
+    MyInt(int value = 0) : value(value) {}
 
-	void SetData(int value) { this->value = value; }
-	int GetData() const { return value; }
+    void SetData(int value) { this->value = value; }
+    int GetData() const { return value; }
 };
+
 
 template <typename T>
 class Node
 {
 private:
-	T data;
-	Node* next;
+    T data;
+    Node* next;
+    Node* prev;
 public:
-	Node(T data) : data(data), next(nullptr) {}
+    Node(T data) : data(data), next(nullptr), prev(nullptr) {}
 
-	T& GetData() { return data; }
-	void SetData(T data) { this->data = data; }
+    T& GetData()
+    {
+        return data;
+    }
 
-	Node* GetNext() const { return next; }
-	void SetNext(Node* next) { this->next = next; }
+    void SetData(T data)
+    {
+        this->data = data;
+    }
+
+    Node* GetNext() const
+    {
+        return next;
+    }
+
+    void SetNext(Node* next)
+    {
+        this->next = next;
+    }
+
+    Node* GetPrev() const
+    {
+        return prev;
+    }
+
+    void SetPrev(Node* prev)
+    {
+        this->prev = prev;
+    }
 };
 
 template <typename T>
@@ -38,9 +65,12 @@ private:
 public:
     Queue() : first(nullptr), last(nullptr), size(0) {}
 
-    size_t Size() const { return size; }
+    size_t Size() const
+    {
+        return size;
+    }
 
-    void Enqueue(T data) // Add to queue list
+    void EnqueueBack(T data)
     {
         Node<T>* newNode = new Node<T>(data);
         if (last == nullptr)
@@ -51,12 +81,30 @@ public:
         else
         {
             last->SetNext(newNode);
+            newNode->SetPrev(last);
             last = newNode;
         }
         size++;
     }
 
-    T& Front() // Get first elem
+    void EnqueueFront(T data)
+    {
+        Node<T>* newNode = new Node<T>(data);
+        if (first == nullptr)
+        {
+            first = newNode;
+            last = newNode;
+        }
+        else
+        {
+            newNode->SetNext(first);
+            first->SetPrev(newNode);
+            first = newNode;
+        }
+        size++;
+    }
+
+    T& Front()
     {
         if (first == nullptr)
         {
@@ -65,7 +113,7 @@ public:
         return first->GetData();
     }
 
-    const T Front() const // for const. object
+    const T Front() const
     {
         if (first == nullptr)
         {
@@ -74,7 +122,25 @@ public:
         return first->GetData();
     }
 
-    void Dequeue() // Delete first from queue
+    T& Back()
+    {
+        if (last == nullptr)
+        {
+            throw runtime_error("Queue is empty!");
+        }
+        return last->GetData();
+    }
+
+    const T Back() const
+    {
+        if (last == nullptr)
+        {
+            throw runtime_error("Queue is empty!");
+        }
+        return last->GetData();
+    }
+
+    void DequeueFront()
     {
         if (first == nullptr)
         {
@@ -82,9 +148,33 @@ public:
         }
         Node<T>* temp = first;
         first = first->GetNext();
+        if (first != nullptr)
+        {
+            first->SetPrev(nullptr);
+        }
         if (first == nullptr)
         {
             last = nullptr;
+        }
+        delete temp;
+        size--;
+    }
+
+    void DequeueBack()
+    {
+        if (last == nullptr)
+        {
+            return;
+        }
+        Node<T>* temp = last;
+        last = last->GetPrev();
+        if (last != nullptr)
+        {
+            last->SetNext(nullptr);
+        }
+        if (last == nullptr)
+        {
+            first = nullptr;
         }
         delete temp;
         size--;
@@ -100,30 +190,32 @@ public:
         }
     }
 };
+
 int main()
 {
-    Queue<MyInt> queue;
-    queue.Enqueue(MyInt(9));
-    queue.Enqueue(MyInt(1));
-    queue.Enqueue(MyInt(5));
-    queue.Enqueue(MyInt(7));
-    queue.Enqueue(MyInt(3));
+    Queue<int> queue; //Int queue
+    queue.EnqueueBack(9);
+    queue.EnqueueBack(1);
+    queue.EnqueueBack(5);
+    queue.EnqueueBack(7);
+    queue.EnqueueBack(3);
 
     for (int i = queue.Size(); i > 0; i--)
     {
-        cout << queue.Front().GetData() << endl;
-        queue.Dequeue();
+        cout << queue.Front() << endl;
+        queue.DequeueFront();
     }
 
-    // int queue
-    Queue<int> intQueue;
-    intQueue.Enqueue(10);
-    intQueue.Enqueue(20);
-    intQueue.Enqueue(30);
+    Queue<string> stringQueue; // String queue
+    stringQueue.EnqueueBack("I");
+    stringQueue.EnqueueBack("Love");
+    stringQueue.EnqueueBack("Windows");
 
-    while (intQueue.Size() > 0)
+    while (stringQueue.Size() > 0)
     {
-        cout << intQueue.Front() << endl;
-        intQueue.Dequeue();
+        cout << stringQueue.Front() << endl;
+        stringQueue.DequeueFront();
     }
+
+
 }
